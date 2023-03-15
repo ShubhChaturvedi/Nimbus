c04e4257662956885b8e9468e421bd08ce816224
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2023-03-15 16:40:04 +0530

    DEV: chat app

--- nimbus/asgi.py
+++ nimbus/asgi.py
@@ -10,7 +10,17 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nimbus.settings')
 
 application = get_asgi_application()
+
+ws_pattern = [
+    path('chat/' ,)
+]
+
+application = ProtocolTypeRouter({
+    'websockets' : URLRouter(ws_pattern)
+})
