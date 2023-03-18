947ee5c4e645041a15493c14011df2bd5736c483
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2023-03-18 15:21:15 +0530

    DEV: chatroom

--- chat/views.py
+++ chat/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def chatIndex(request):
-    return HttpResponse("Welcome to chat")
+    return render(request , 'dashboard/messages.html')
 
 def roomName(request, room_name):
     return HttpResponse("Welcome to chat room")
