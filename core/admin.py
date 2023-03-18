fb4e15ca36bf0ad87880e267ad0effb90129e42b
Krish Agrawal <printf.krish@gmail.com>
2023-03-18 15:03:24 +0530

    added key model to

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+@admin.register(Key)
+class KeyAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'Website_Name', 'Meta_Desc', 'Favicon', 'Logo', 'Email', 'Facebook', 'Instagram', 'Twitter')
